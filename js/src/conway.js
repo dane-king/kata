bada22d2e93d8d03983a8c79f0313e5f4a36f25b
dane-king <kingd9@gmail.com>
2015-09-27 13:44:42 -0400

    Adding js files

--- js/src/conway.js
+++ js/src/conway.js
@@ -1,1 +1,20 @@
-var World=function(){};
+function World(rows, columns) {
+	var _this = this;
+	this.cells = new Cells(rows,columns);
+
+	this.hasCells = function() {
+		return _this.cells.rows!==0 || _this.cells.rows!==0;
+	};
+
+	this.isEmpty = function() {
+		return !_this.hasCells();
+	};
+}
+
+function Cells(rows, columns) {
+	var getValidNumber=function(num){
+		return Number.isInteger(rows)?rows:0;	
+	};
+	this.rows = getValidNumber(rows);
+	this.columns = getValidNumber(columns);
+}
